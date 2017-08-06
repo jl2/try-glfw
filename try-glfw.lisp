75e5e36f2a4f92d1719d7ea6866309f97db87e06
Jeremiah LaRocco <jeremiah.larocco@gmail.com>
2017-08-05 20:33:18 -0600

    Start from scratch.

--- try-glfw.lisp
+++ try-glfw.lisp
@@ -45,100 +45,21 @@ gl_FragColor = mix(gl_Color, vec4(vec3(gl_SecondaryColor), 1.0), 0.5);
   (declare (ignore window))
   (set-viewport w h))
 
-(defun initialize-opengl ()
-  (gl:enable :line-smooth :polygon-smooth
-             :depth-test :depth-clamp :alpha-test)
-
-  (let ((buffers (gl:gen-buffers 2)))
-    (setf (vertex-buffer  shader-example) (elt buffers 0)
-          (index-buffer  shader-example) (elt buffers 1)))
-  (gl:bind-buffer :array-buffer (vertex-buffer  shader-example))
-  (let ((arr (gl:alloc-gl-array :float 12))
-        (verts #(-0.5f0 -0.5f0 0.0f0 
-                 -0.5f0 0.5f0 0.0f0 
-                 0.5f0 -0.5f0 0.0f0 
-                 0.5f0 0.5f0 0.0f0)))
-    (dotimes (i (length verts))
-      (setf (gl:glaref arr i) (aref verts i)))
-    (gl:buffer-data :array-buffer :static-draw arr)
-    (gl:free-gl-array arr))
-
-  ;; 0 is always reserved as an unbound object.
-  (gl:bind-buffer :array-buffer 0)
-
-  ;; An element array buffer stores vertex indices. We fill it in the
-  ;; same way as an array buffer.
-  (gl:bind-buffer :element-array-buffer (index-buffer  shader-example))
-  (let ((arr (gl:alloc-gl-array :unsigned-short 6))
-        (indexes #(0 2 1 1 2 3)))
-    (dotimes (i (length indexes))
-      (setf (gl:glaref arr i) (aref indexes i)))
-    (gl:buffer-data :element-array-buffer :static-draw arr)
-    (gl:free-gl-array arr))
-  (gl:bind-buffer :element-array-buffer 0)
-
-  ;; Vertex array objects manage which vertex attributes are
-  ;; associated with which data buffers. 
-  (setf (vertex-array  shader-example) (gl:gen-vertex-array))
-  (gl:bind-vertex-array (vertex-array  shader-example))
-
-  ;; To associate our VBO data with this VAO, we bind it, specify
-  ;; which vertex attribute we want to associate it with, and specify
-  ;; where the data comes from.
-  (gl:bind-buffer :array-buffer (vertex-buffer  shader-example))
-  ;; In this program, we use attribute 0 for position. If you had
-  ;; per-vertex normals, you could use a different attribute for those
-  ;; as well.
-  (gl:enable-vertex-attrib-array 0)
-  ;; Using a null pointer as the data source indicates that we want
-  ;; the vertex data to come from the currently bound array-buffer.
-  (gl:vertex-attrib-pointer 0 3 :float nil 0 (cffi:null-pointer))
-
-  ;; To associate an element array with this VAO, all we need to do is
-  ;; bind the element array buffer we want to use.
-  (gl:bind-buffer :element-array-buffer (index-buffer  shader-example))
-
-  ;; Once we're done, we can unbind the VAO, and rebind it when we want to render it.
-  (gl:bind-vertex-array 0)
-
-  ;; A program object is a collection of shader objects to be used
-  ;; together in a single pipeline for rendering objects. To create a
-  ;; program, you first create the individual shaders. Then you attach
-  ;; the shaders to the program and link the program together.
-  (let ((vs (gl:create-shader :vertex-shader))
-        (fs (gl:create-shader :fragment-shader)))
-    (setf (vertex-shader  shader-example) vs)
-    (setf (fragment-shader  shader-example) fs)
-    (gl:shader-source vs *shader-vao-vertex-program*)
-    (gl:compile-shader vs)
-    (gl:shader-source fs *shader-vao-fragment-program*)
-    (gl:compile-shader fs)
-    ;; If the shader doesn't compile, you can print errors with:
-    ;; (print (gl:get-shader-info-log vs))
-    ;; (print (gl:get-shader-info-log fs))
-
-    (setf (program shader-example) (gl:create-program))
-    ;; You can attach the same shader to multiple different programs.
-    (gl:attach-shader (program shader-example) vs)
-    (gl:attach-shader (program shader-example) fs)
-    ;; Don't forget to link the program after attaching the
-    ;; shaders. This step actually puts the attached shader together
-    ;; to form the program.
-    (gl:link-program (program shader-example))
-    ;; If we want to render using this program object, or add
-    ;; uniforms, we need to use the program. This is similar to
-    ;; binding a buffer.
-    (gl:use-program (program shader-example))))
-
 (defun main ()
   ;; Graphics calls on OS X must occur in the main thread
   (with-body-in-main-thread ()
-    (with-init-window (:title "Window test" :width 600 :height 400)
+    (with-init-window (:title "Window test"
+                              :width 600
+                              :height 400
+                              :opengl-profile :opengl-core-profile
+                              :context-version-major 3
+                              :context-version-minor 2
+                              :opengl-forward-compat t
+                              :resizable nil)
+
       (setf %gl:*gl-get-proc-address* #'get-proc-address)
       (set-key-callback 'quit-on-escape)
       (set-window-size-callback 'update-viewport)
-      (initialize-opengl)
-
       (gl:clear-color 0 0 0 0)
       (set-viewport 600 400)
       (loop until (window-should-close-p)
